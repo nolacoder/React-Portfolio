dcfe3ec5003eae6a8bd86c2ab50208ca09f509ec
Jared <jjohns3@tulane.edu>
2022-11-02 21:30:17 -0500

    Set up connections between PorfolioContainer and pages

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,5 +1,9 @@
 import React, { useState }from 'react';
-import Header from './Header'
+import Header from './Header';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function PortfolioContainer() {
 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,1 +1,7 @@
+import React from 'react';
 
+function Resume () {
+    
+}
+
+export default Resume
